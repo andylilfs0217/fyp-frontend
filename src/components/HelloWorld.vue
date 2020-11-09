226899e86c903516b4cf5590f4abbd50aa08a7a1
jojo <jojo@aigniter.com>
2020-11-09 18:04:10 +0800

    camera

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <span>
+    <div class="camera">
+      <video 
+        ref="webcamRef"
+        autoplay
+        class="feed"
+        id="camera"
+        width="1080"
+        height="720"       
+      />
+      <canvas 
+        id="canvas"
+        width="1080"
+        height="720"
+      ></canvas>
+    </div>
+    <button @click="togglePoseNet">PoseNet is now on: {{ isPosenetOn }}</button>
+  </span>
 </template>
 
 <script>
+//import * as ml5 from "ml5";
+import { drawKeypoints, drawSkeleton } from "../utilities.js";
+
+const confidence = 0.25;
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "camera2",
+  data() {
+    return {
+      video: null,
+      poseNet: null,
+      pose: null,
+      skeleton: null,
+      isPosenetOn: false,
+      canvas: null,
+      ctx: null,
+    };
+  },
+  methods: {
+    init() {
+      // Get access to the web camera
+      if (
+        "mediaDevices" in navigator &&
+        "getUserMedia" in navigator.mediaDevices
+      ) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+          const videoPlayer = document.querySelector("video");
+          videoPlayer.srcObject = stream;
+          videoPlayer.play();
+        });
+      } else {
+        alert("Cannot get Media Devices");
+      }
+
+      this.setup();
+    },
+    setup() {
+      // Get the video HTML component to video var
+      this.video = document.getElementById("camera");
+      // Get canvas HTML element for drawing later
+      this.canvas = document.getElementById("canvas");
+      this.ctx = this.canvas.getContext("2d");
+
+      // Load poseNet with video, and call func modelLoaded()
+      //this.poseNet = ml5.poseNet(this.video, this.modelLoaded);
+      // Change poseNet to on (check obj watch.isPosenetOn)
+      this.isPosenetOn = true;
+    },
+    gotPoses(poses) {
+      // Get all poses and corressponding coordinates to var pose and skeleton
+      if (poses && poses.length > 0) {
+        this.pose = poses[0].pose;
+
+        // Reset canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Draw pose and skeleton on canvas
+        drawKeypoints(this.pose.keypoints, confidence, this.ctx, 1);
+        console.log(this.pose);
+        drawSkeleton(this.pose.keypoints, confidence, this.ctx, 1);
+      }
+    },
+
+    modelLoaded() {
+      console.log("poseNet ready");
+    },
+
+    togglePoseNet() {
+      this.isPosenetOn = !this.isPosenetOn;
+    },
+  },
+  watch: {
+    isPosenetOn() {
+      if (this.isPosenetOn) {
+        // Turn on poseNet and loop func gotPoses()
+        this.poseNet.on("pose", this.gotPoses);
+      } else {
+        this.poseNet.removeListener("pose", this.gotPoses);
+      }
+    },
+  },
+  mounted() {
+    this.init();
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.feed {
+  display: block;
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  background-color: #171717;
+  box-shadow: 6px 6px 12px 0px rgba(0, 0, 0, 0.35);
+  height:auto;
+  position:absolute
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#canvas{
+  display: block;
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  height:auto;
+  position:absolute
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
