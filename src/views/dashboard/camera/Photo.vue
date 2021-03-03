6924afbbcc0f4f2502226284c4cb534c89ee0266
Andy Li <andylilfs@gmail.com>
2021-03-03 22:00:44 +0800

    feat(photo): obtain poses from pictures

--- src/views/dashboard/camera/Photo.vue
+++ src/views/dashboard/camera/Photo.vue
@@ -6,8 +6,18 @@
       label="Image input"
       v-model="images"
       multiple
-    ></v-file-input>
-    <v-btn @click="submitImage" color="primary">Submit</v-btn>
+    >
+      <template v-slot:append>
+        <v-btn @click="onSubmit" color="primary">Submit</v-btn>
+      </template>
+    </v-file-input>
+    <!-- Display images -->
+    <v-row>
+      <v-col cols="2" v-for="(image, idx) in images" :key="idx">
+        <v-img :ref="`image-${idx}`" :src="getImageUrl(image)" aspect-ratio="1">
+        </v-img>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -18,36 +28,40 @@ import runningImage from "../../../assets/model/running.jpg";
 import { drawKeypoints, drawSkeleton } from "@/utils/utilities.js";
 
 export default {
-  data() {
-    return {
-      images: null,
-      poseNet: null,
-      poses: []
-    };
-  },
-  mounted() {},
-  methods: {
+  data: () => ({
+    images: null,
+    poseNet: null,
+    poses: []
+  }),
+  mounted() {
     // Initialize Posenet
-    async submitImage() {
-      const options = {
-        imageScaleFactor: 1,
-        outputStride: 16,
-        flipHorizontal: false,
-        minConfidence: 0.05,
-        maxPoseDetections: 5,
-        scoreThreshold: 0.05,
-        nmsRadius: 20,
-        detectionType: "multiple",
-        multiplier: 0.75
-      };
-      this.poseNet = await ml5.poseNet(this.modelReady, options);
-      this.poseNet.on("pose", function(results) {
-        console.log(results);
-      });
+    const options = {
+      imageScaleFactor: 1,
+      outputStride: 16,
+      flipHorizontal: false,
+      minConfidence: 0.5,
+      maxPoseDetections: 5,
+      scoreThreshold: 0.5,
+      nmsRadius: 20,
+      detectionType: "multiple",
+      multiplier: 0.75
+    };
+    this.poseNet = ml5.poseNet(() => console.log("Model loaded"), options);
+    this.poseNet.on("pose", this.gotPoses);
+  },
+  methods: {
+    // Activated on submit
+    async onSubmit() {
+      this.poses = [];
+      for (let idx = 0; idx < this.images.length; idx++) {
+        this.poseNet.multiPose(this.$refs[`image-${idx}`][0].image);
+      }
     },
-    modelReady() {
-      console.log("Model loaded");
-      this.poseNet.multiPose(runningImage);
+    // Compute the image url for display
+    getImageUrl: (image) => URL.createObjectURL(image),
+    // Avtivated when detect a pose
+    gotPoses(results) {
+      this.poses.push(results);
     }
   }
 };
